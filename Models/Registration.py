7cdf2e387f1fa3e6feba749cb95c0d8cd454ee9c
Oscar Rosberg <oscros-7@student.ltu.se>
2019-11-26 12:32:41 +0100

    Made changes to RegisterForm class, more changes needed

--- Models/Registration.py
+++ Models/Registration.py
@@ -2,10 +2,11 @@ from flask import Flask, render_template, flash, redirect, url_for, sessions, lo
 from wtforms import Form, StringField, TextAreaField, PasswordField, validators
 import hashlib, uuid
 
-class ReigsterForm(Form):
-    first_name = StringField('First Name', [validators.Length(min=1, max=50), validators.InputRequired])
+
+class RegisterForm(Form):
+    first_name = StringField('First Name', [validators.Length(min=1, max=50), validators.InputRequired()])
     last_name = StringField('Last Name', [validators.Length(min=1, max=50), validators.InputRequired()])
-    email = StringField('Email', [validators.email, validators.Length(min=1, max=50)])
-    city = StringField('City', validators.Length(min=1, max=50))
-    postal_code = StringField('Postal Code', validators.Length(min=1, max=50))
-    phone = StringField('Phone Number', validators.Length(min=1, max=50))
+    email = StringField('Email', [validators.Email("Enter a valid email"), validators.Length(min=1, max=50), validators.InputRequired()])
+    city = StringField('City', [validators.Length(min=1, max=50), validators.InputRequired()])
+    postal_code = StringField('Postal Code', [validators.Length(min=5, max=5), validators.InputRequired()])
+    phone = StringField('Phone Number', [validators.Length(min=1, max=50)])
--- app.py
+++ app.py
@@ -1,5 +1,10 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, flash, redirect, url_for, sessions, logging, request
 import pymysql.cursors
+from Models import Registration
+from wtforms import Form, StringField, TextAreaField, PasswordField, validators
+
+from Models.Registration import RegisterForm
+
 """connection = pymysql.connect(host='localhost',
                              user='oscar',
                              password='hej',
@@ -24,9 +29,15 @@ app = Flask(__name__)
 def hello_world():
     return render_template('index.html')
 
-@app.route('/register')
+
+@app.route('/register', methods=['GET', 'POST'])
 def register():
-    return render_template('register.html')
+    form = RegisterForm(request.form)
+    if request.form == 'POST' and form.validate():
+        return render_template('register.html', form=form)
+    return render_template('register.html', form=form)
+
+
 
 if __name__ == '__main__':
-    app.run(Debug = True)
+    app.run(Debug=True)
